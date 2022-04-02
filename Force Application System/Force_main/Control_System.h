6152194a8f34d722a3b92e26bc3eed2e6eec68e9
Luka Emrashvili <59441844+Luka-Emra@users.noreply.github.com>
2022-04-02 12:16:07 -0700

    Revert "LCD & x_axis"
    This reverts commit ea824cbbf0332aae6eda8aa431e37ca90adfdf17.

---
+++ Force Application System/Force_main/Control_System.h
@@ -0,0 +1,61 @@
+
+#include <PID_v1.h>
+#include "Z_Pos_Control.h"
+#include "Force_calibration.h"
+
+ 
+ double Force_Reading;
+ static boolean newDataReady = 0;
+/////////////////////////////////////////////////////////////////////////
+//PID parameters
+double Kp=.1, Ki=1.25, Kd=10; 
+
+         //sensor input,  Controller output, Setpoint ,coefficents         
+PID myPID(&Force_Reading, &Output_Position, &Force, Kp, Ki, Kd, DIRECT);
+
+///////////////////////////////////////////////////////////////////////// 
+void Control_setup()
+{
+ 
+  //Turn the PID on
+  myPID.SetMode(AUTOMATIC);
+  //Adjust PID values
+  myPID.SetTunings(Kp, Ki, Kd);
+}
+
+/////////////////////////////////////////////////////////////////////
+
+void Control_loop()
+{
+
+  if (Force_Reading > Force){ // this will change the motor direction up on down
+    M_direction = LOW;        // depending on if we are above or below our target force
+  }
+  else{
+    M_direction = HIGH;
+  }  
+  
+  if (LoadCell.update()) newDataReady = true;
+  if (newDataReady) {
+      Force_Reading = LoadCell.getData();
+      
+      Serial.print(Force_Reading);
+      Serial.print(',');
+      Serial.print(Output_Position);
+      Serial.print(',');  
+      Serial.print(M_direction);
+      Serial.print(',');  
+      Serial.print(SPEED);      
+      Serial.print(',');   
+      Serial.println();
+      newDataReady = 0;
+  }
+  //PID calculation to determine new output
+  myPID.Compute();
+  
+
+  //Serial.println(Output_Position);
+  //Serial.print(" ");  
+  //Serial.println(Force);
+  delay(10);
+}
---
+++ Load_cell_test/Load_cell_test.ino
@@ -0,0 +1,13 @@
+
+#include "Calibration.h"
+#include "Excel_Data_Streamer.h"
+
+
+
+void setup() {
+ calibration_setup();
+}
+
+void loop() {
+ Calibration(); 
+}
