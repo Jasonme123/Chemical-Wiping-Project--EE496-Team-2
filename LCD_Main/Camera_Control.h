46c8654696ce82476086377d3c09f3f553398ca7
Luka Emrashvili <59441844+Luka-Emra@users.noreply.github.com>
2022-04-06 15:35:19 -0700

    camera code

--- LCD_Main/Camera_Control.h
+++ LCD_Main/Camera_Control.h
@@ -1,4 +1,3 @@
-
 //Hardware Design Info
 //https://elliotthilaire.net/how-to-connect-your-camera-to-arduino/
 
@@ -7,20 +6,24 @@
 
 //Pulse Info
 //http://stereo.jpn.org/eng/sdm/ca1.htm
- 
 
-void Camera_setup()
-{
-// initialize the digital pins as an output.
-pinMode(shutter_control, OUTPUT);
-//pinMode(focus_control, OUTPUT);
+void Camera_setup() {
+  // put your setup code here, to run once:
+  // initialize the digital pins as an output.
+  pinMode(shutter_control, OUTPUT);
+  //pinMode(focus_control, OUTPUT);
 }
- 
-void Photo()
-{
-analogWrite(bright_pin, Photo_Brightness); //light up enclosure to Photo brightness
-digitalWrite(shutter_control, HIGH);   //Push Buttom
-delay(150);               //Full Shoot Mode
-digitalWrite(shutter_control, LOW);    //Release Button
-analogWrite(bright_pin, Norm_Brightness); //light up enclosure to normal brightness
+
+void Photo() {
+      delay(1000);
+      analogWrite(bright_pin, Photo_Brightness);// 128 //light up enclosure to Photo brightness
+      digitalWrite(shutter_control, HIGH);   //Push Buttom
+      delay(30);
+      digitalWrite(shutter_control, LOW);    //Release Button
+      delay(150);               //Full Shoot Mode
+      digitalWrite(shutter_control, HIGH);   //Push Buttom
+      delay(150);
+      digitalWrite(shutter_control, LOW);   //Push Buttom
+      delay(1000); 
+      analogWrite(bright_pin, Norm_Brightness); // 0 //light up enclosure to normal brightness
 }
