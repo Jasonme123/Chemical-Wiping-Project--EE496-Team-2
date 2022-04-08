df612c73caf62a5fa69bdb29b4b5eec87b120699
Jasonme123 <jasonbabayev8@gmail.com>
2022-04-08 12:36:06 -0700

    pause button added

--- LCD_Main/Safety_Check.h
+++ LCD_Main/Safety_Check.h
@@ -1,10 +1,35 @@
+// define saftey things here
 
 void safety_Check(){
  
- 
+ //If loadcell not found
  if (scale.wait_ready_retry(10)){
    Serial.println("HX711 not found.");
  }
   
 
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//Hard Pause Button
+void HardPause(){
+  delay(1000);
+  int Play_state = !Play_state;
+  
+  if(Play_state = false){
+  TIMER1_INTERRUPTS_OFF;
+  TIMER2_INTERRUPTS_OFF;
+  digitalWrite(X_ENABLE_PIN, HIGH);
+  }
+
+  if(Play_state = true){
+  TIMER1_INTERRUPTS_ON;
+  TIMER2_INTERRUPTS_ON;
+  digitalWrite(X_ENABLE_PIN, LOW);
+  }
+}
+
+void Pause_Button_Setup() {
+  pinMode(hard_pause, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(hard_pause), HardPause, CHANGE);
 }
